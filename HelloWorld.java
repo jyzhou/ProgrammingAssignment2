b88a7385bf183193ea8741e330c26f5a7d249967
Jieyi Zhou <jyzhou1990@gmail.com>
2016-03-19 03:29:18 -0600

    Added files via upload

---
+++ HelloWorld.java
@@ -0,0 +1,11 @@
+public class HelloWorld{
+  public HelloWorld(){
+      System.out.println("Hello World, Git and Azure!");
+  }
+  
+  public static void main(String[] args){
+	HelloWorld HW = new HelloWorld();
+  }
+
+}
+
